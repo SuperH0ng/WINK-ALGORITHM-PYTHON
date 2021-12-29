cdf9008400c768f4d72f726baa3f556c48a1d710
SuperH0ng <81635179+SuperH0ng@users.noreply.github.com>
2021-12-29 20:42:25 +0900

    백준 7569번

--- 9th week/백준 7569번(토마토)2트 성공.py
+++ 9th week/백준 7569번(토마토)2트 성공.py
@@ -1,4 +1,3 @@
-import copy
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -43,42 +42,3 @@ for i in range(H):
         break
 
 print(answer)
-
-
-# def ripeTomato(x, y, z):
-#     queue = deque([[x, y, z]])
-#     global c
-#     c = copy.deepcopy(c)
-#     c[x][y][z] = 10
-#     while queue:
-#         p = queue.popleft()
-#         for i in range(6):
-#             if p[0] + dx[i] < 0 or p[0] + dx[i] >= len(c) or p[1] + dy[i] < 0 or p[1] + dy[i] >= len(c[0]) or p[2] + dz[i] < 0 or p[2] + dz[i] >= len(c[0][0]):
-#                 continue
-#             if c[p[0] + dx[i]][p[1] + dy[i]][p[2] + dz[i]] == 0:
-#                 c[p[0] + dx[i]][p[1] + dy[i]][p[2] +
-#                                               dz[i]] = c[p[0]][p[1]][p[2]] + 1
-#                 queue.append([p[0] + dx[i], p[1] + dy[i], p[2] + dz[i]])
-#             elif c[p[0] + dx[i]][p[1] + dy[i]][p[2] + dz[i]] == 1:
-#                 return c[p[0]][p[1]][p[2]] + 1 - 10
-#             else:
-#                 pass
-#     return -1
-
-
-# def days(H, N, M):
-#     answer = 0
-#     for i in range(H):
-#         for j in range(N):
-#             for l in range(M):
-#                 if c[i][j][l] == 0:
-#                     result = ripeTomato(i, j, l)
-#                     if result == -1:
-#                         return -1
-#                     else:
-#                         if result > answer:
-#                             answer = result
-#     return answer
-
-
-# print(days(H, N, M))
