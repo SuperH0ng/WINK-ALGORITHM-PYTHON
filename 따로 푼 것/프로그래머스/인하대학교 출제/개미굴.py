ebdd51daabe885d8db49a4dedbf155a4b6e445f0
SuperH0ng <ghdtmdgus100@gmail.com>
2022-02-14 10:45:55 +0900

    인하대 출제

--- 따로 푼 것/프로그래머스/인하대학교 출제/개미굴.py
+++ 따로 푼 것/프로그래머스/인하대학교 출제/개미굴.py
@@ -1,4 +1,3 @@
-import sys
 from collections import deque
 
 def solution(road, transport):
@@ -14,5 +13,31 @@ def solution(road, transport):
             roadDic[r[1]] = [r[0]]
         else :
             roadDic[r[1]].append(r[0])
-            
-    return roadDic
+    print(roadDic)
+    for t in transport :
+        answer += count(roadDic, t)
+        print(answer)
+        
+    return answer
+
+def count(dic, trans) :
+    dest = trans[1]
+    queue = deque([trans])
+    cnt = 0
+
+    while queue :
+        x, y= queue.popleft()
+        print(x,y)
+        if x not in dic :
+            return -1
+        if dest in dic[x] :
+            return cnt + 1
+        
+        for i in range(len(dic[x])) :
+            for j in range(len(dic[dic[x][i]])) :
+                queue.append([dic[x][i], dic[dic[x][i]][j]])
+        cnt += 1
+    
+    return cnt
+
+print(solution([[6, 3], [3, 1], [3, 2], [2, 4], [5, 1]], [[6, 3], [5, 2], [5, 4]]))
