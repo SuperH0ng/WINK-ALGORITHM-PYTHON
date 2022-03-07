2f221ef08eb064022df565a8a3a1ef527aec193c
SuperH0ng <ghdtmdgus100@gmail.com>
2022-03-08 01:27:48 +0900

    백준 10951번

---
+++ 따로 푼 것/백준/백준 10951번(A+B - 4).py
@@ -0,0 +1,18 @@
+# ipt = map(int, input().split())
+# while (ipt != "") :
+#     print(sum(ipt))
+#     ipt = map(int, input().split())
+
+# # x, y = map(int, input().split())
+# # print(x+y)
+
+# # try :
+# #     while(True) :
+# #         print(sum(map(int, ipt)))
+
+
+while(True) :
+    try :
+        print(sum(map(int, input().split())))
+    except :
+        break
