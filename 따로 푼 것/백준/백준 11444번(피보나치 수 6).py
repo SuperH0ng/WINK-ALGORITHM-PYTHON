98039bc86bcbff9d352447e5be690635575f15cd
SuperH0ng <ghdtmdgus100@gmail.com>
2022-03-24 17:18:30 +0900

    백준 11444번

---
+++ 따로 푼 것/백준/백준 11444번(피보나치 수 6).py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(1000000007)
+
+def mulMtx(a, b) :
+    tmp = [[0, 0], [0, 0]]
+    tmp[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % 1000000007
+    tmp[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % 1000000007
+    tmp[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % 1000000007
+    tmp[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % 1000000007
+    return tmp
+
+def fibo(f) :
+    if f == 1 :
+        return [[1, 1], [1, 0]]
+    
+    mtx = fibo(f//2)
+    if f % 2 == 0 :
+        return mulMtx(mtx, mtx)
+    else :
+        return mulMtx(mulMtx(mtx, mtx), fibo(1))
+
+n = int(input())
+print(fibo(n-1)[0][0]) if n > 1 else print(1)
